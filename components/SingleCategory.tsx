0461e0040dddc2aab0096adcb7d8a0089ff4547f
Rubin Elezi <rubinelezi0@gmail.com>
2022-02-25 22:52:41 +0100

    tailwind support added, singleMenuItem redisign

--- components/SingleCategory.tsx
+++ components/SingleCategory.tsx
@@ -1,37 +1,51 @@
 import styles from "../styles/EventItem.module.css";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import {Category} from "../types/Category"
+import { Category } from "../types/Category";
+import { url } from "inspector";
 
 interface SingleCategory {
-  category: Category
+  category: Category;
   businessID: number;
 }
 
-export default function SingleCategory({ category, businessID }: SingleCategory) {
+export default function SingleCategory({
+  category,
+  businessID,
+}: SingleCategory) {
   const router = useRouter();
   const handleClick = (slug: number) => {
-      router.replace(`${businessID}/category/${slug}`);
+    router.replace(`${businessID}/category/${slug}`);
   };
-  
+
   return (
-    <div className={styles.cart} key={category.id} id={category.CategorySlug} onClick={() => handleClick(category.id)}>
-      <div className="relative item-detail">
-        <Image
-          className={styles.img}
-          src={
+    <div
+      className="flex flex-col items-center justify-center max-w-sm mx-auto my-10 cursor-pointer"
+      key={category.id}
+      id={category.CategorySlug}
+      onClick={() => handleClick(category.id)}
+    >
+      <div
+        className="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md"
+        style={{
+          backgroundImage: `url(${
             category.image
               ? category.image[0].formats.medium.url
               : "/images/event-default.png"
-          }
-          layout={"fill"}
-          objectFit={"cover"}
-        />
-      </div>
+          })`,
+        }}
+      ></div>
+      <div className="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+        <h3 className="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">
+          {category.name}
+        </h3>
 
-      <div className={styles.info}>
-        <h1>{category.name}</h1>
-        <p>{category.description}</p>
+        <div className="flex items-center justify-center px-3 py-2 bg-gray-200 dark:bg-gray-700">
+          <span className="font-bold text-ellipsis text-center text-gray-800 dark:text-gray-200">
+            {category.description}
+          </span>
+          {/* <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">EXPLORE!</button> */}
+        </div>
       </div>
     </div>
   );
