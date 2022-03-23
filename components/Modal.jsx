3a31fdfe8fd30c06940eb1cd1c28d4b793fe3b15
Rubin <rubinelezi0@gmail.com>
2022-03-23 13:59:28 +0100

    modal close only on x

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -10,7 +10,7 @@ export default function  Modal({show, onClose, children, title}){
         onClose();
     }
     const content = show ? (
-        <div className={styles.overlay}>
+        <div className={`${styles.overlay}`}>
             <div className={styles.modal}>
                 <div className={styles.header}>
                     <a href="#" onClick={handleClose}>
