6e2101175c6e47c4270e515ace334d91b08b1fad
Rubin <rubinelezi0@gmail.com>
2022-03-23 12:42:41 +0100

    modal close only on x

--- components/SingleMenuItem.tsx
+++ components/SingleMenuItem.tsx
@@ -15,49 +15,52 @@ export default function SingleMenuItem({ menuItem }: SingleMenuItem) {
 
   const [showModal, setShowModal] = useState(false)
   return (
-    <div
-      className="flex flex-row border-2 border-transparent bg-neutral-700 rounded-lg m-3 h-44 p-2"
-      key={menuItem.id}
-      onClick={() => handleClick(menuItem.id)}
-    >
-      <div className="w-1/2 h-full">
-        <img
-          className="object-cover w-40 h-full rounded-lg"
-          src={
-            menuItem.images
-              ? menuItem.images[0].formats.medium.url
-              : "/images/event-default.png"
-          }
-        />
-      </div>
+      <>
+        <div
+            className="flex flex-row border-2 border-transparent bg-neutral-700 rounded-lg m-3 h-44 p-2"
+            key={menuItem.id}
+            onClick={() => handleClick(menuItem.id)}
+        >
+          <div className="w-1/2 h-full">
+            <img
+                className="object-cover w-40 h-full rounded-lg"
+                src={
+                  menuItem.images
+                      ? menuItem.images[0].formats.medium.url
+                      : "/images/event-default.png"
+                }
+            />
+          </div>
 
-      <div className="ml-2 w-1/2 h-full">
-        <h1 className="font-medium">{menuItem.name}</h1>
-        <p className="text-clip overflow-auto h-20 font-thin">{menuItem.description}</p>
-        <p className="text-orange-600 font-bold">
-          {menuItem.price} {menuItem.currency}
-        </p>
-      </div>
+          <div className="ml-2 w-1/2 h-full">
+            <h1 className="font-medium">{menuItem.name}</h1>
+            <p className="text-clip overflow-auto h-20 font-thin">{menuItem.description}</p>
+            <p className="text-orange-600 font-bold">
+              {menuItem.price} {menuItem.currency}
+            </p>
+          </div>
+        </div>
 
-      <Modal show={showModal} onClose={() => setShowModal(false)} title={menuItem.name}>
-        <div>
-          <img
-              className="object-cover w-full max-h-60 rounded-lg"
-              src={
-                menuItem.images
-                    ? menuItem.images[0].formats.medium.url
-                    : "/images/event-default.png"
-              }
-          />
-        </div>
-        <div className="ml-2 h-full">
-          <h1 className="font-medium">{menuItem.name}</h1>
-          <p className="text-orange-600 font-bold">
-            {menuItem.price} {menuItem.currency}
-          </p>
-          <p className="text-clip overflow-auto h-44 text-black font-thin">{menuItem.description}</p>
-        </div>
-      </Modal>
-    </div>
+        <Modal show={showModal} onClose={() => setShowModal(false)} title={menuItem.name}>
+          <div>
+            <img
+                className="object-cover w-full max-h-60 rounded-lg"
+                src={
+                  menuItem.images
+                      ? menuItem.images[0].formats.medium.url
+                      : "/images/event-default.png"
+                }
+            />
+          </div>
+          <div className="ml-2 h-full">
+            <h1 className="font-medium">{menuItem.name}</h1>
+            <p className="text-orange-600 font-bold">
+              {menuItem.price} {menuItem.currency}
+            </p>
+            <p className="text-clip overflow-auto h-44 text-black font-thin">{menuItem.description}</p>
+          </div>
+        </Modal>
+      </>
+
   );
 }
